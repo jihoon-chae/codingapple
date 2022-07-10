36b7f0d6f287414a83fc325cc2f0aa4c2aac9eb1
Jihoon Chae <93469760+jihoon-chae@users.noreply.github.com>
2022-07-10 16:11:49 +0900

    MongoDB 셋팅하기 (무료 호스팅도 받아보자)

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express(); // 서버를 띄우기 위한 기본 세팅
 
 const bodyParser = require("body-parser"); // bodyParser라이브러리 사용
-app.use(bodyParser.urlencoded({ extended: true }));// bodyParser라이브러리 사용
+app.use(bodyParser.urlencoded({ extended: true })); // bodyParser라이브러리 사용
 
-app.listen(8080, function () {
-  // 서버를 열 수 있음(서버띄울 포트번호, 띄운후 실행할 코드)
-  console.log("listening on 8080");
-});
+// MongoDB와 연결
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb+srv://hoonn:1234@cluster0.8me9d.mongodb.net/?retryWrites=true&w=majority",
+  function (에러, client) {
+    if (에러) return console.log(에러);
+    //서버띄우는 코드 여기로 옮기기
+    app.listen("8080", function () {
+      console.log("listening on 8080");
+    });
+  }
+);
+
 
 app.get("/pet", function (요청, 응답) {
   // get(경로, 함수) // 펫상품 보여주기
@@ -28,7 +38,7 @@ app.get("/", function (요청, 응답) {
 app.get("/write", function (요청, 응답) {
   응답.sendFile(__dirname + "/write.html");
 });
- 
+
 app.post("/add", function (요청, 응답) {
   // add경로로 POST 요청을 하면 ~을 해주세요
   응답.send("전송완료");
